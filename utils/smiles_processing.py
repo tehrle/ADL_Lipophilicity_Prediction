26522d102f8fd9af3a763e4b8f20efcc92a88d82
tehrle <t.ehrle@gmx.ch>
2024-11-26 23:16:39 +0100

    changed from dgllife to PyG for graph generation.

--- utils/smiles_processing.py
+++ utils/smiles_processing.py
@@ -4,22 +4,18 @@ from rdkit.Chem import MolFromSmiles, AddHs
 from rdkit.Chem.Descriptors import MolWt
 from rdkit.Chem.Draw import MolToImage
 from rdkit.Chem.Crippen import MolLogP
-from dgllife.utils import mol_to_bigraph, CanonicalAtomFeaturizer, CanonicalBondFeaturizer
+from torch_geometric.utils import from_smiles
 from torch_geometric.data import Data
 import torch
 
 # TODO: Add Docstrings for class
 
 class SMILESConverter:
-    def __init__(self, smiles, logP, image_size=128,
-                 atom_featurizer=CanonicalAtomFeaturizer(),
-                 bond_featurizer=CanonicalBondFeaturizer()):
+    def __init__(self, smiles, logP, image_size=128):
 
         self.smiles = smiles
         self.logP = logP
         self.image_size = image_size
-        self.atom_featurizer = atom_featurizer
-        self.bond_featurizer = bond_featurizer
 
         self.mol = self.smiles_to_mol()
         self.graph = self.mol_to_graph()
@@ -92,31 +88,18 @@ class SMILESConverter:
         return MolToImage(self.mol, size=(self.image_size, self.image_size))
 
     def mol_to_graph(self):
-        # Create DGL graph
-        dgl_graph = mol_to_bigraph(
-            mol=self.mol,
-            node_featurizer=self.atom_featurizer,  # Atom features from DGL-Life-Sciences
-            edge_featurizer=self.bond_featurizer  # Bond features from DGL-Life-Sciences
-        )
 
-        # Extract node features from DGL graph
-        node_features = dgl_graph.ndata['h']  # Node features: Shape [num_nodes, num_node_features]
+        # Create PyG Graph
+        graph = from_smiles(self.smiles)
 
-        # Extract edge features from DGL graph
-        edge_features = dgl_graph.edata['e']  # Edge features: Shape [num_edges, num_edge_features]
+        # Convert Node mat into float
+        graph.x = graph.x.float()
 
-        # Extract edge index (source and destination nodes of edges)
-        src, dst = dgl_graph.edges()  # Returns two tensors: source nodes and destination nodes
-        edge_index = torch.stack([src, dst], dim=0).to(torch.int64)  # Shape [2, num_edges]
+        # Convert edge attributes into float
+        graph.edge_attr = graph.edge_attr.float()
 
-        # Create PyTorch Geometric Data object
-        graph = Data(
-            x=node_features,            # Node features
-            edge_index=edge_index,      # Edge indices
-            edge_attr=edge_features,    # Edge features
-            smiles=self.smiles,         # Saves SMILES in PyG graph
-            y=torch.tensor(self.logP, dtype=torch.float).view(1, -1) # add corresponding logP value to graph
-        )
+        # Add logP value to graph
+        graph.y = torch.tensor(self.logP, dtype=torch.float).view(1, -1)
 
         return graph
 

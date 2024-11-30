b2de1e68985a3f8fce48f2513d6a25f87d3b5c19
tehrle <t.ehrle@gmx.ch>
2024-11-30 19:07:16 +0100

    updated attentiveFP script.

--- scripts/attentivefp_model_v01.py
+++ scripts/attentivefp_model_v01.py
@@ -1,10 +1,11 @@
 # import necessary packages
+import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 from torch_geometric.loader import DataLoader
 from torch.optim import Adam
-from torch_geometric.nn import AttentiveFP
+from torch_geometric.nn import AttentiveFP, summary
 import os
 import sys
 import logging
@@ -12,8 +13,12 @@ import logging
 # logging settings
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.INFO,
+    stream=sys.stdout       # Ensure logging outputs are printed into SLURM output file
 )
+
+# Redirect stderr to stdout
+sys.stderr = sys.stdout
 
 # settings for hpc:
 # change wd to current folder to match relative paths
@@ -23,7 +28,6 @@ os.chdir(script_dir)    # change the current directory to the specified director
 # insert top dir to system path for hpc
 sys.path.insert(0, '../')
 from utils import split_dataset, train_graph_model, plot_history, predict_graph_model, RMSELoss, plot_logp_correlation
-
 #=======================================================================================================================
 
 #
@@ -75,11 +79,19 @@ model = AttentiveFP(
     dropout=0.2
 )
 
-# model summary
-# need to figure out how to print model summary using torch_geometric.nn.summary
+# Print Model Summary
+num_nodes = 5
+node_dim = 9
+num_edges = 8
+edge_dim = 3
+batch_size = 32
 
+dummy_x = torch.randn(num_nodes, node_dim)
+dummy_edge_index = torch.randint(0, num_nodes, (2, num_edges))
+dummy_edge_attr = torch.randn(num_edges, edge_dim)
+dummy_batch = torch.randint(0, batch_size, (num_nodes,))
 
-
+print(summary(model, dummy_x, dummy_edge_index, dummy_edge_attr, dummy_batch))
 #=======================================================================================================================
 
 #
@@ -92,7 +104,7 @@ logging.info('Starting to train model...')
 optimizer = Adam(model.parameters(), lr=3e-4)
 
 # Number of epochs for training
-num_epochs = 100
+num_epochs = 2
 
 metrics = train_graph_model(
     model=model,
@@ -101,7 +113,8 @@ metrics = train_graph_model(
     loss_fn=nn.MSELoss(),
     train_loader=train_loader,
     val_loader=val_loader,
-    device=device
+    device=device,
+    use_edge_attr=True
 )
 
 logging.info(f'Finished: Trained model for {num_epochs} epochs')
@@ -126,7 +139,7 @@ training_lod_df = pd.DataFrame(metrics)
 
 # save trainings log
 training_lod_df.to_csv(
-    '../deep_learning_outputs/training_logs/train_history_attentiveFp_v01.csv',
+    '../deep_learning_outputs/training_logs/train_history_attentiveFP_v01.csv',
     index=False,
     header=True
 )
@@ -145,7 +158,8 @@ logging.info('Evaluating model on test set...')
 y_pred_tensor, y_test_tensor = predict_graph_model(
     model=model,
     test_loader=test_loader,
-    device=device
+    device=device,
+    use_edge_attr=True
 )
 
 logging.info(f'Shape of y_pred_tensor: {y_pred_tensor.shape}')
@@ -166,6 +180,21 @@ logging.info(f'Mean Squared Error: {mse_loss.item()}')
 logging.info(f'Mean Absolute Error: {mae_loss.item()}')
 logging.info(f'Root Mean Squared Error: {rmse_loss.item()}')
 
+# Save metrics on test set as csv-file
+test_metrics_df = pd.DataFrame({
+    'test_mse': [mse_loss.item()],
+    'test_mae': [mae_loss.item()],
+    'test_rmse': [rmse_loss.item()]
+})
+
+test_metrics_csv = '../deep_learning_outputs/model_evaluation/attentiveFP_test_evaluation_v01.csv'
+
+test_metrics_df.to_csv(
+    test_metrics_csv,
+    index=False,
+    header=True
+)
+
 # Convert Metrics to np.ndarray for plotting
 y_pred_array = y_pred_tensor.flatten().numpy()
 y_test_array = y_test_tensor.flatten().numpy()
@@ -173,8 +202,18 @@ y_test_array = y_test_tensor.flatten().numpy()
 logging.info(f'Shape of y_pred_array: {y_pred_array.shape}')
 logging.info(f'Shape of y_test_array: {y_test_array.shape}')
 
+npz_file = '../deep_learning_outputs/model_evaluation/attentiveFp_predicted_values_v01.npz'
+
+np.savez(
+    npz_file,
+    y_pred=y_pred_array,
+    y_test=y_test_array
+)
+
+logging.info(f'Successfully saved labels/ predicted values of test set as {npz_file}')
+
 # Plot predicted vs. True logP Values
-correlation_file = f'../deep_learning_outputs/figures/attentiveFP_correlation_plot_v01.png'
+correlation_file = f'../deep_learning_outputs/model_evaluation/attentiveFP_correlation_plot_v01.png'
 
 plot_logp_correlation(
     file=correlation_file,
